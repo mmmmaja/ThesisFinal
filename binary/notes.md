8e1a47a6b2f7a5ad8f2bbdeba7c4397e57b7a96e
mjgoj <mj.gojska@gmail.com>
2024-04-14 11:35:54 +0200

    new commit

---
+++ binary/notes.md
@@ -0,0 +1,30 @@
+# Binary Classification
+* 1 (stable)
+* 2 (decliner)
+
+### 1. Feature Selection
+* Random Forest (Non-linear feature selection)
+* L1 Logistic Regression (Linear feature selection)
+
+### 2. Data Integration
+
+### 3. Training Models
+(different models can bring out different interpretations)
+* EBM (Interpretable model)
+* Logistic Regression (with Elastic Net - l1 and l2 regularization)
+
+### 4. Pathway Analysis & Interpretation
+* Proteomics - String DB (has pathway enrichment analysis)
+* Metabolomics - MetaboAnalyst / IMPALA
+* Lipidomics - need to find correct names first (reach out to Valentin)
+
+Tasks:
+* PLS vs logistic regression
+* sPLS vs lasso / elastic net
+* **concatenated sPLS vs Diablo**
+* Tune PLS-DA model
+
+**22.04** make presentation, what done so far and highlights for the future
+
+Pipeline for Experimentation
+- nextFlow
--- helper.py
+++ helper.py
@@ -1,111 +1,79 @@
+import math
 import sys
-import pandas as pd
+
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib_venn import venn3, venn2
-from sklearn.datasets import load_iris
-from sklearn.feature_selection import SelectKBest, VarianceThreshold
-from sklearn.feature_selection import f_classif
-from sklearn.feature_selection import mutual_info_classif
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.feature_selection import RFE
-from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from imblearn.over_sampling import SMOTE
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, recall_score, precision_score, \
+    f1_score
+from sklearn.preprocessing import StandardScaler
 
-"""
-This file consists of helper methods for feature selection
-as well as Ven diagrams for visualizing the intersection of features (3 or 2 sets)
-and a method for getting the training and testing set from the dataset
-"""
+path = "C:/Users/mjgoj/Desktop/THESIS/data/final_dataset_split.xlsx"
+xls = pd.ExcelFile(path)
+
+lipidomics = pd.read_excel(xls, "Lipidomics")
+metabolomics = pd.read_excel(xls, "Pareto Metabolomics")
+proteomics = pd.read_excel(xls, "Pareto Proteomics")
 
 
-def ANOVA_correlation_filtering(k, X_, Y_):
+def plot_confusion_matrix(y_test_, y_prediction_):
     """
-    ANOVA F-value statistics to select the top k features
-    that have the highest correlation to the target variable
-    :param k: the number of features to select
-    :param X_: the feature matrix
-    :param Y_: the class labels
+    Plot the confusion matrix of the model
+    :param y_test_: True labels
+    :param y_prediction_: Predicted labels
     """
-    selector = SelectKBest(f_classif, k=k)  # Selects the top 10 features
-    selector.fit_transform(X_, Y_)
-    # Return the names of the columns that were picked
-    features = X_.columns[selector.get_support()]
-    print("Number of features after ANOVA filtering ", len(features))
-    return features
-
-
-def variance_filtering(X_, threshold=0.01):
-    """
-    Removing features with low variance.
-    It is the only non-supervised feature selection method. It is important as it prevents overfitting
-    :param X_: the feature matrix
-    :param threshold: the threshold for variance
-    """
-    sel = VarianceThreshold(threshold=threshold)
-    sel.fit_transform(X_)
-    # Return the names of the columns that were picked
-    features = X_.columns[sel.get_support()]
-    print("Number of features after variance filtering ", len(features))
-    return features
-
-
-def mutual_information_filtering(k, X_, Y_):
-    """
-    Mutual Information based Feature Selection (MIFS)
-    Mutual information statistics to select the top k features
-    that have the highest correlation to the target variable
-
-    Measures how much information the presence/absence of a feature
-    contributes to making the correct prediction on Y
-
-    :param k: the number of features to select
-    :param X_: the feature matrix
-    :param Y_: the class labels
-    """
-
-    selector = SelectKBest(mutual_info_classif, k=k)
-    selector.fit_transform(X_, Y_)
-    # Return the names of the columns that were picked
-    features = X_.columns[selector.get_support()]
-    print("Number of features after mutual information filtering ", len(features))
-    return features
-
-
-def recursive_feature_elimination(k, X_, Y_, estimator=DecisionTreeClassifier()):
-    """
-    Wrapper approach to feature selection
-    Starts with all features and removes the least important one at each iteration
-    :param k: number of features to select
-    :param X_: the feature matrix
-    :param Y_: the class labels
-    :param estimator: the model used to evaluate the importance of features
-        DecisionTreeClassifier
-        RandomForestClassifier
-
-    """
-
-    selector = RFE(estimator, n_features_to_select=k, step=1)
-    selector.fit_transform(X_, Y_)
-    # Return the names of the columns that were picked
-    features = X_.columns[selector.get_support()]
-    print("Number of features after recursive feature elimination ", len(features))
-    return features
-
-
-def venn_diagram(set_lists, set_labels, title=None):
-    """
-    Create a Venn diagram for 2 or 3 sets
-    :param set_lists: list of sets of features
-    :param set_labels: list of the filtering methods used
-    :param title: the title of the plot
-    """
-    if len(set_lists) == 2:
-        venn2(set_lists, set_labels)
-    elif len(set_lists) == 3:
-        venn3(set_lists, set_labels)
-    if title:
-        plt.title(title)
+    # Create the confusion matrix
+    cm = confusion_matrix(y_test_, y_prediction_)
+    # Plot the confusion matrix
+    if len(np.unique(y_prediction_)) == 2:
+        labels = ["Stable", "Decliner"]
+    else:
+        labels = ["Stable", "Slow Decliner", "Fast Decliner"]
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(cmap="GnBu")
     plt.show()
+
+
+def get_omics_type(feature_name):
+
+    if feature_name in lipidomics.columns:
+        return "Lipidomics"
+    elif feature_name in metabolomics.columns:
+        return "Metabolomics"
+    elif feature_name in proteomics.columns:
+        return "Proteomics"
+    return None
+
+def standardize(X):
+    """
+    Standardize the data using the StandardScaler
+    This is crucial for regularized models
+    (otherwise features with larger scales dominate the regularization)
+    :param X: Data
+    """
+    scaler = StandardScaler()
+    return scaler.fit_transform(X)
+
+
+def evaluate_model(y_pred, y_true):
+    """
+    Evaluate the model
+    :param y_pred: Predicted labels
+    :param y_true: True labels
+    """
+
+    accuracy = accuracy_score(y_true, y_pred)
+    print(f'Accuracy of the model: {accuracy:.4f}')
+    recall = recall_score(y_true, y_pred, average='weighted')
+    print(f'Recall of the model: {recall:.4f}')
+    precision = precision_score(y_true, y_pred, average='weighted')
+    print(f'Precision of the model: {precision:.4f}')
+    f1 = f1_score(y_true, y_pred, average='weighted')
+    print(f'F1 score of the model: {f1:.4f}')
+
+    # Plot the confusion matrix
+    plot_confusion_matrix(y_true, y_pred)
 
 
 def get_train_test_split_RIDs():
@@ -138,7 +106,113 @@ def get_train_test_split_dataset(dataset):
     return training_set, testing_set
 
 
-if __name__ == "__main__":
-    xls = pd.ExcelFile("C:/Users/mjgoj/Desktop/THESIS/data/final_dataset_split.csv")
+def resampling(x, y):
+    """
+    Resample the dataset to balance the classes
+
+    Initial dataset is very imbalanced, with the majority class being the "Stable" class
+    Therefore the tree always predicted the majority class
+    """
+    smote = SMOTE()
+    x_resampled, y_resampled = smote.fit_resample(x, y)
+    print("Resampled set shape: ")
+    for dataset in [x_resampled, y_resampled]:
+        print(dataset.shape)
+    print("Resampled set class distribution: \n", y_resampled.value_counts())
+    return x_resampled, y_resampled
+
+
+def pareto_scaling(dataset_):
+    """
+    https://www.rdocumentation.org/packages/MetabolAnalyze/versions/1.3.1/topics/scaling
+    https://uab.edu/proteomics/metabolomics/workshop/2014/statistical%20analysis.pdf
+    Pareto scaling is often used in metabolomics.
+    It scales data by dividing each variable by the square root of the standard deviation,
+    so that each variable has variance equal to 1.
+    :param dataset_: original non-normalized dataset (can have RID and class labels)
+    :return: normalized dataset
+    """
+    # Maintain a list of dropped columns to add them back after scaling
+    dropped_columns = []
+    if "RID" in dataset_.columns:
+        RID = dataset_["RID"]
+        dataset_ = dataset_.drop("RID", axis=1)
+        dropped_columns.append(RID)
+    if "ThreeClass" in dataset_.columns:
+        three_class = dataset_["ThreeClass"]
+        dataset_ = dataset_.drop("ThreeClass", axis=1)
+        dropped_columns.append(three_class)
+    if "TwoClass" in dataset_.columns:
+        dataset_ = dataset_.drop("TwoClass", axis=1)
+
+    # Scale the dataset
+    for feature in dataset_.columns:
+        std = dataset_[feature].std()
+        scaling_factor = math.sqrt(std)
+        dataset_[feature] = dataset_[feature] / scaling_factor
+    for col in dropped_columns:
+        dataset_ = pd.concat([dataset_, col], axis=1)
+    return dataset_
+
+
+def concatenate_data(
+        lipidomics, metabolomics, proteomics,
+        resample_train=False, resample_test=False, num_classes=3):
+    """
+    Load the concatenated dataset and split it into training and testing set
+    :param lipidomics: the lipidomics dataset
+    :param metabolomics: the metabolomics dataset
+    :param proteomics: the proteomics dataset
+    :param resample_train: whether to resample the training set
+    :param resample_test: whether to resample the testing set
+    :param num_classes: 3 for the multiclass classification, 2 for the binary classification
+    :return:
+    """
+
+    # Merge the datasets on RID
+    integrated_dataset = pd.merge(lipidomics, metabolomics, on="RID", how="inner")
+    integrated_dataset = pd.merge(integrated_dataset, proteomics, on="RID", how="inner")
+
+    # Get the train and test split
+    train, test = get_train_test_split_dataset(integrated_dataset)
+
+    # Get the class labels
+    if num_classes == 2:
+        Y_train_, Y_test_ = train["TwoClass"], test["TwoClass"]
+    else:
+        Y_train_, Y_test_ = train["ThreeClass"], test["ThreeClass"]
+
+    # Drop columns that have following substrings in their names
+    columns_to_drop = ["ThreeClass", "TwoClass", "RID"]
+    for col in train.columns:
+        if columns_to_drop[0] in col or columns_to_drop[1] in col or columns_to_drop[2] in col:
+            train = train.drop(col, axis=1)
+            test = test.drop(col, axis=1)
+
+    if resample_train:
+        train, Y_train_ = resampling(train, Y_train_)
+    if resample_test:
+        test, Y_test_ = resampling(test, Y_test_)
+    return train, test, Y_train_, Y_test_
+
+
+def load_data():
+    """
+    Load the lipidomics, metabolomics and proteomics datasets from the predefined path
+    """
+    path = "C:/Users/mjgoj/Desktop/THESIS/data/final_dataset_split.xls"
+    xls = pd.ExcelFile(path)
+    lipidomics = pd.read_excel(xls, "Lipidomics")
     metabolomics = pd.read_excel(xls, "Metabolomics")
-    get_train_test_split_dataset(metabolomics)
+    proteomics = pd.read_excel(xls, "Proteomics")
+    return lipidomics, metabolomics, proteomics
+
+
+if __name__ == "__main__":
+    l, m, p = load_data()
+    X_train, X_test, Y_train, y_test = concatenate_data(l, m, p, resample_train=True, resample_test=False)
+    substr = "ThreeClass"
+    cols = X_train.columns
+    # Get the name of the features where substr is in the name
+    print([col for col in cols if substr in col])
+    # print(Y_train)
