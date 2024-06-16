f3dcb7dba2092c4bb988e3e868359a00a6b81fb8
mjgoj <mj.gojska@gmail.com>
2024-06-16 11:02:35 +0200

    README finished, code done

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
+# Prediction of Cognitive Decline Trajectories on Multi-Omics Data
 
 ## Table of Contents
+- [Introduction](#introduction)
 - [Project Structure](#project-structure)
+- [Installation](#installation)
+
+### Introduction
+
+In this project, I evaluated various methodologies
+for predicting cognitive decline trajectories
+using multi-omics data from the **Alzheimer's Disease Neuroimaging Initiative (ADNI)**.
+
+The classification of cognitive decline trajectories developed by _Laroche et al._ 
+is used as the target variable for the predictive modeling tasks
+The classification includes three classes: _Stable_, _Slow Decliner_, _Fast Decliner_
+or a binary classification task with _Stable_ and _Decliner_ classes.
+
+![image info](results/images/class_distribution.png)
+
+
+I tested various feature selection techniques as a preprocessing step before predictive modeling, including:
+- **Boruta method**
+- **Recursive Feature Elimination**
+- **Mutual Information Feature Selection**
+- **Anova F-Value Feature Selection**
+
+Furthermore, I tested various machine learning models for binary and multiclass classification tasks.
+This includes:
+- **Logistic Regression with L1 regularization**
+- **Random Forest**
+- **(s)PLS-DA**
+- **AdaBoost**
+- **DIABLO**
 
 ### Project Structure
-
 
 ```plaintext
 ├── binary_classification   # Includes model testing and biomarker selection methods
@@ -23,4 +53,26 @@
 │   └──  images             # Images used for plotting
 │ 
 └── README.md               # Project README
+```
+
+### Installation
+
+**Python Dependencies**
+
+Install the required Python packages using pip:
+
+```bash
+# Clone the repository
+git clone https://github.com/mmmmaja/ThesisFinal.git
+
+# Install required packages
+pip install -r requirements.txt
+```
+
+**R Dependencies
+Make sure you have R installed on your system.**
+Install the required R packages using the following commands in an R console:
+    
+```R
+install.packages(c("readxl", "dplyr", "mixOmics", "reshape2"))
 ```
--- data/dataset_creation.R
+++ data/dataset_creation.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 "
-In this file I create a new dataset using OMICS_older_toclasses 
+In this file I create a new dataset using OMICS_older_toclasses
 matching to samples by RID
 
 final_dataset:
@@ -10,9 +10,9 @@ RID | lcmm_classes_3 | lcmm_classes_3 | Samples...
 
 dataset <- OMICS_data
 # Add the RID column which is the rownumber
-dataset$RID = rownames(dataset)
+dataset$RID <- rownames(dataset)
 # Move the RID column to the first position  
-dataset = dataset[, c(ncol(dataset), 1:(ncol(dataset)-1))]
+dataset <- dataset[, c(ncol(dataset), 1:(ncol(dataset)-1))]
 
 # mapping: stable: 0, decliner: 1
 dataset$TwoClass <- ifelse (dataset$TwoClass== "Stable", 0, 1)
@@ -33,12 +33,12 @@ dataset <- left_join(dataset, ThreeClassData, by = "RID")
 # Rename the newly added class column correctly
 colnames(dataset)[which(colnames(dataset) == "lcmm_classes3")] <- "ThreeClass"
 # Move the ThreeClass column to the second position
-dataset = dataset[, c(1, ncol(dataset), 2:(ncol(dataset)-1))]
+dataset <- dataset[, c(1, ncol(dataset), 2:(ncol(dataset)-1))]
 
 print("Dataset Done!")
 
 # Save the final dataset
-path = "C:/Users/mjgoj/Desktop/THESIS/data/final_dataset.RData"
+path <- "C:/Users/mjgoj/Desktop/THESIS/data/final_dataset.RData"
 save(dataset, file = path)
 
 print("Dataset saved successfully!")
@@ -52,13 +52,13 @@ Splitting the dataset into different omics types
 source("C:/Users/mjgoj/Desktop/THESIS/R/utils.R")
 
 # Each omics type inherits first3 columns from the dataset
-proteomics_final = dataset[, c("RID", "TwoClass", "ThreeClass")]
-metabolomics_final = dataset[, c("RID", "TwoClass", "ThreeClass")]
-lipidomics_final = dataset[, c("RID", "TwoClass", "ThreeClass")]
+proteomics_final <- dataset[, c("RID", "TwoClass", "ThreeClass")]
+metabolomics_final <- dataset[, c("RID", "TwoClass", "ThreeClass")]
+lipidomics_final <- dataset[, c("RID", "TwoClass", "ThreeClass")]
 
 for (i in 4 : ncol(dataset)) {
-  feature = colnames(dataset)[i]
-  data_type = find_omics_type(feature)
+  feature <- colnames(dataset)[i]
+  data_type <- find_omics_type(feature)
   if (is.null(data_type)) {
     print(paste("Feature", feature, "not found in any dataset"))
   }
@@ -78,18 +78,18 @@ for (i in 4 : ncol(dataset)) {
 }
 
 # Metabolomics, remove categorical variables
-categorical_cols = sapply(metabolomics_final, function(x) length(unique(x))) < 2
+categorical_cols <- sapply(metabolomics_final, function(x) length(unique(x))) < 2
 print("Categorical columns in metabolomics")
 print(colnames(metabolomics_final)[categorical_cols])
 
 # Remove the categorical columns
-metabolomics_final = metabolomics_final[, !categorical_cols]
+metabolomics_final <- metabolomics_final[, !categorical_cols]
 print(metabolomics_final$TAG_HIGH_PYRUVATE)
 
 # Bar plot for each omics type
-counts = c(length(colnames(proteomics_final)), length(colnames(metabolomics_final)), length(colnames(lipidomics_final)))
-names = c("Proteomics", "Metabolomics", "Lipidomics")
-df = data.frame(names, counts)
+counts <- c(length(colnames(proteomics_final)), length(colnames(metabolomics_final)), length(colnames(lipidomics_final)))
+names <- c("Proteomics", "Metabolomics", "Lipidomics")
+df <- data.frame(names, counts)
 
 # Plot the bar plot
 ggplot(df, aes(x = names, y = counts, fill = names)) +
@@ -104,9 +104,9 @@ ggplot(df, aes(x = names, y = counts, fill = names)) +
 
 # Save in the CSV file to different sheets
 library(openxlsx)
-path = "C:/Users/mjgoj/Desktop/THESIS/data/final_dataset_split.xls"
+path <- "C:/Users/mjgoj/Desktop/THESIS/data/final_dataset_split.xls"
 
-workbook = createWorkbook()
+workbook <- createWorkbook()
 addWorksheet(workbook, "Proteomics")
 writeData(workbook, "Proteomics", proteomics_final)
 
@@ -118,16 +118,16 @@ writeData(workbook, "Lipidomics", lipidomics_final)
 
 saveWorkbook(workbook, path, overwrite = TRUE)
 
-path = "C:/Users/mjgoj/Desktop/THESIS/data/final_dataset.RData"
+path <- "C:/Users/mjgoj/Desktop/THESIS/data/final_dataset.RData"
 # Save all the datasets
 save(proteomics_final, metabolomics_final, lipidomics_final, dataset, file = path)
 
 # Extract RIDs from training and testing datasets
 
-RID_train = rownames(training)
-text_RID_train = paste(RID_train, collapse = ", ")
-RID_test = rownames(testing)
-text_RID_test = paste(RID_test, collapse = ", ")
+RID_train <- rownames(training)
+text_RID_train <- paste(RID_train, collapse = ", ")
+RID_test <- rownames(testing)
+text_RID_test <- paste(RID_test, collapse = ", ")
 
 
 # Define the file path
@@ -157,22 +157,22 @@ RID_test = rownames(testing)
 
 # Handling the train set
 # Extract the labels for the training dataset
-OMICS_older_toclasses_train = OMICS_older_toclasses %>% filter(RID %in% RID_train)
-ThreeClass_train = OMICS_older_toclasses_train$lcmm_classes3
-training$ThreeClass = ThreeClass_train
-training$RID = RID_train
+OMICS_older_toclasses_train <- OMICS_older_toclasses %>% filter(RID %in% RID_train)
+ThreeClass_train <- OMICS_older_toclasses_train$lcmm_classes3
+training$ThreeClass <- ThreeClass_train
+training$RID <- RID_train
 
 # And now the test set
-OMICS_older_toclasses_test = OMICS_older_toclasses %>% filter(RID %in% RID_test)
-ThreeClass_test = OMICS_older_toclasses_test$lcmm_classes3
-testing$ThreeClass = ThreeClass_test
-testing$RID = RID_test
+OMICS_older_toclasses_test <- OMICS_older_toclasses %>% filter(RID %in% RID_test)
+ThreeClass_test <- OMICS_older_toclasses_test$lcmm_classes3
+testing$ThreeClass <- ThreeClass_test
+testing$RID <- RID_test
 
 # SAVE as xls
 library(openxlsx)
-path = "C:/Users/mjgoj/Desktop/THESIS/data/wtf_train_test.xls"
+path <- "C:/Users/mjgoj/Desktop/THESIS/data/wtf_train_test.xls"
 
-workbook = createWorkbook()
+workbook <- createWorkbook()
 addWorksheet(workbook, "Test")
 writeData(workbook, "Test", testing)
 
